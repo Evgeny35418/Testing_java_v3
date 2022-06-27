51b6ab296c23f697cc23f71dbae6199e0d0d803a
Evgeny35418 <107989117+Evgeny35418@users.noreply.github.com>
2022-06-27 15:22:21 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/testing/java/auto/MyStart.java
+++ sandbox/src/main/java/testing/java/auto/MyStart.java
@@ -1,8 +1,28 @@
 package testing.java.auto;
 
+import java.sql.SQLOutput;
+
 public class MyStart{
    public static void main( String[] args){
 
        System.out.println("Hello,World!");
+       speak("Russia");
+       speak("Albert");
+       speak("Piter");
+       double B = 5;
+       System.out.println("Площадь квадрата со стороной " + B + "= "+ number(B));
+       double A = 4;
+       double C = 6;
+       System.out.println("Площадь прямоугольника со сторонами "+ A +" и " + C + "= "+ number2(A,C));
+
+  }
+  public static void speak(String i){
+     System.out.println("Hello, " + i);
+  }
+  public static double number(double i){
+     return i*i;
+  }
+  public static double number2( double A, double B){
+     return A*B;
   }
 }
