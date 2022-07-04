3dbf7200c6aca17c778691375d7cde130bf20484
Evgeny35418 <107989117+Evgeny35418@users.noreply.github.com>
2022-07-04 08:08:44 +0300

    модифицируем код теста для веб -приложения
    - Перенесли некоторые действия в предусловия(   @BeforeMethod )

--- addressbook-web-tests/src/test/java/testing/java/auto/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/testing/java/auto/addressbook/GroupCreationTests.java
@@ -20,10 +20,6 @@ public class GroupCreationTests {
 
     wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
     js = (JavascriptExecutor) wd;
-  }
-
-  @Test
-  public void testGroupCreation() throws Exception {
     wd.get("http://localhost/addressbook/group.php");
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
@@ -32,6 +28,11 @@ public class GroupCreationTests {
     wd.findElement(By.name("pass")).clear();
     wd.findElement(By.name("pass")).sendKeys("secret");
     wd.findElement(By.xpath("//input[@value='Login']")).click();
+  }
+
+  @Test
+  public void testGroupCreation() throws Exception {
+
     wd.findElement(By.name("new")).click();
     wd.findElement(By.name("group_name")).click();
     wd.findElement(By.name("group_name")).clear();
