bad6f4f6890d72d8a3900e4ca0f2b23acae13e92
Evgeny35418 <107989117+Evgeny35418@users.noreply.github.com>
2022-07-05 18:04:26 +0300

    Задание 4. Создаем тест для создания контактов в адресной книге
    разделил 2 теста по разным классам

--- addressbook-web-tests/src/test/java/testing/java/auto/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/testing/java/auto/addressbook/GroupCreationTests.java
@@ -15,15 +15,7 @@ public class GroupCreationTests extends TestBase {
     returnToGroupPage();
   }
 
-  @Test
-  public void testNewUserCreation() throws Exception {
 
-    gotoAddNewPage();
-    fillAddNewForm(new NewUser("Ivan", "ivanov", "autotest", "saint-peterburg", "999999999", "jekaqa@mail.ru"));
-    creatingANewUser();
-
-
-  }
 
 
 }
