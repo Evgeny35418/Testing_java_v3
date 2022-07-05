7eebac8d6bdec4849d672910813bbb5172d8372a
Evgeny35418 <107989117+Evgeny35418@users.noreply.github.com>
2022-07-05 12:48:10 +0300

    Задание 4. Создаем тест для создания контактов в адресной книге

---
+++ addressbook-web-tests/src/test/java/testing/java/auto/addressbook/NewUser.java
@@ -0,0 +1,43 @@
+package testing.java.auto.addressbook;
+
+public class NewUser {
+  private final String firstName;
+  private final String lastName;
+  private final String company;
+  private final String address;
+  private final String telephone;
+  private final String email;
+
+  public NewUser(String firstName, String lastName, String company, String address, String telephone, String email) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.company = company;
+    this.address = address;
+    this.telephone = telephone;
+    this.email = email;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public String getCompany() {
+    return company;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getTelephone() {
+    return telephone;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+}
