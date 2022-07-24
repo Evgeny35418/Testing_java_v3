f48d808b2658d2eae1264948f8577c2b03ec0e91
Evgeny35418 <107989117+Evgeny35418@users.noreply.github.com>
2022-07-24 08:06:11 +0300

    изменения

--- addressbook-web-tests/src/test/java/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/tests/GroupCreationTests.java
@@ -15,21 +15,16 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation()  {
     appM.getNavigationHelper().goToGroupPage();
     List<GroupData> before = appM.getGroupHelper().getGroupList();
-    GroupData group = new GroupData("test1", null, null);
+    GroupData group = new GroupData("test3", null, null);
     appM.getGroupHelper().createGroup(group);
     List<GroupData> after = appM.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(),before.size() +1 );
 
 
-    int max = 0;
-    for(GroupData g: after){
-      if(g.getId()> max){
-        max = g.getId();
-      }
-    }
-    Comparator<? super GroupData> byId= (Comparator<GroupData>) (o1, o2) -> Integer.compare(o1.getId(), o2.getId());
-    int max1 = after.stream().max(byId).get().getId();
-    group.setId(max1);
+
+
+
+    group.setId( after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group);
     Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
   }
