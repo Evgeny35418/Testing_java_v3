08dcaca00048baab5ca2966c238f1ea6c1c3e2a5
Evgeny35418 <107989117+Evgeny35418@users.noreply.github.com>
2022-07-26 14:00:03 +0300

    Задание 9

--- addressbook-web-tests/src/test/java/tests/ContactModificationTest.java
+++ addressbook-web-tests/src/test/java/tests/ContactModificationTest.java
@@ -4,6 +4,7 @@ import model.ContactData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -25,6 +26,9 @@ public class ContactModificationTest extends TestBase {
 
     before.remove(before.size()-1);
     before.add(contact);
-    Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
+    Comparator<? super ContactData> byId= (g1,g2) -> Integer.compare(g1.getId(), g2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(before,after);
   }
 }
